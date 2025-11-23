2ddbfd2cc7e76c7fce88a5597bab54abdbeaf07f
tongbaominh <Bmtong04@gmail.com>
2025-11-23 18:04:21 +0100

    Fix: RequestMapping Import hinzugefügt

--- Content Creation Workflow Platform/src/main/java/htw/webtech/todo_app/ContentPlanerController.java
+++ Content Creation Workflow Platform/src/main/java/htw/webtech/todo_app/ContentPlanerController.java
@@ -1,6 +1,7 @@
 package htw.webtech.todo_app;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDateTime;
 import java.util.Arrays;
